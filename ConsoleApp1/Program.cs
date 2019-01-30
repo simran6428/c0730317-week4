b15ea69c659fd60ea3279f49ff5bff7302a9cb99
visualstudio <visualstudio@122-PC17>
2019-01-30 11:59:24 -0500

    this can be

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,17 +11,58 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            countryside blue = new countryside();
         }
     }
-        class Village
+    class Village
+    {
+        // Node is an ADT
+        // what KIND of DATA do we need in a NODE?
+        public Village nextVillage;
+        public String Villagename;
+        public String previousVillage;
+        public bool isAstrildeHere = false;
+
+    }
+
+    class countryside
+    {
+        Village Maple = new Village();
+        Village Toronto = new Village();
+        Village Ajax = new Village();
+        Village First;
+        Village Last;
+        Village Temp;
+
+
+
+        public void Launch()
         {
-            // Node is an ADT
-            // what KIND of DATA do we need in a NODE?
-            public Village nextVillage;
-            public String Villagename;
-            public bool isAstrildeHere = false;
+            Village First = Maple;
+            Village Last = Ajax;
+            Maple.Villagename = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.Villagename = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = "Maple";
+            Ajax.Villagename = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage =" Toronto";
+
+            Console.WriteLine(this.displayMap());
 
         }
-    
+        public string displayMap()
+        {
+            string listofCities = "";
+            // we need to print ALL the cities in your Area
+            
+                listofCities = listofCities + First.Villagename + "------- ";
+                    Temp = First.nextVillage;
+            listofCities = listofCities + Temp.Villagename+ "-------" ;
+            
+            return listofCities;
+        }
+    }
 }
