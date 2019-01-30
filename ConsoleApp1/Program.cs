934255b7f83598b13bb9375b4831c5fdea0d7ab8
visualstudio <visualstudio@122-PC17>
2019-01-30 10:27:26 -0500

    s

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     class Program
+
     {
         static void Main(string[] args)
         {
+
         }
     }
+        class Village
+        {
+            // Node is an ADT
+            // what KIND of DATA do we need in a NODE?
+            public Village nextVillage;
+            public String Villagename;
+            public bool isAstrildeHere = false;
+
+        }
+    
 }
